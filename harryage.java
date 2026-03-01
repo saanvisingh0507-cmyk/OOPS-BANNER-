1bf4f941b30fd0042db265e3a149dce0c5c4a91c
saanvisingh0507-cmyk <saanvi.singh0507@gmail.com>
2026-03-01 19:18:12 +0530

    Delete harryage.java

--- harryage.java
+++
@@ -1,9 +0,0 @@
-class harryage{
-     public static void main (String[] args){
-	       String name = "Harry";
-		   int age = 2024 - 2000;
-		   System.out.println("Harry was born in 2000");
-		   System.out.println("The current year is 2024");
-		   System.out.println("Harry's age is" + age);
-	}
-}
