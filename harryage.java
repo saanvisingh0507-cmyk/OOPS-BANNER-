a81e0c15e6f076beb0a5bb8e03a9e02930ab8a4a
saanvisingh0507-cmyk <saanvi.singh0507@gmail.com>
2026-03-01 18:53:31 +0530

    Initial commit

---
+++ harryage.java
@@ -0,0 +1,9 @@
+class harryage{
+     public static void main (String[] args){
+	       String name = "Harry";
+		   int age = 2024 - 2000;
+		   System.out.println("Harry was born in 2000");
+		   System.out.println("The current year is 2024");
+		   System.out.println("Harry's age is" + age);
+	}
+}
