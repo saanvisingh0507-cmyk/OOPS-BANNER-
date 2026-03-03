4fa5c60c6d973e227747f319b06c456621cf068c
saanvisingh0507-cmyk <saanvi.singh0507@gmail.com>
2026-03-03 08:37:07 +0530

    Delete sammarks.java

--- sammarks.java
+++
@@ -1,9 +0,0 @@
-class sammarks{
-     public static void main(String[] args){
-	       String name = "Sam";
-		   int Math=94,Physics=95,Chemistry=96;
-		   int totalmarks=Math+Physics+Chemistry;
-		   int average=(totalmarks)/3;
-		   System.out.println("Sam's average marks in PCM is " + average);
-	}
-}
