792680dfa3d9d8db5e89c0c549ca47f18d493e9f
saanvisingh0507-cmyk <saanvi.singh0507@gmail.com>
2026-03-05 22:19:09 +0530

    ADDED

---
+++ calc.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class calc {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number1: ");
+        double number1 = sc.nextDouble();
+
+        System.out.print("Enter number2: ");
+        double number2 = sc.nextDouble();
+
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double multiplication = number1 * number2;
+        double division = number1 / number2;
+
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "+ number1 + " and " + number2 + " is " + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+    }
+}
